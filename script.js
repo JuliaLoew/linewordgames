c48d6b541ae6bc32da00e996073a964eb44221f8
unknown <julia.loew@gmx.net>
2024-06-04 23:36:12 +0200

    if else Ergaenzung

--- script.js
+++ script.js
@@ -3,13 +3,41 @@ console.log(process.argv);
 const args = process.argv.slice(2);
 
 
-console.log("test");
+// Zufallseingabe vom Computer
+const elements = ['Stein', 'Papier', 'Schere'];
 
-const Computereingabe = "Schere";
+function getRandomElement(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
 
-    if (args == Computereingabe) {
-        console.log('Unentschieden');
-    }
+const randomElement = getRandomElement(elements);
+console.log(randomElement);
+
+
+// Prüfen wer gewonnen hat
+    if (args == randomElement) {
+        console.log('Unentschieden');} 
+        
+    else if (args == "Schere", randomElement == "Papier") {
+        console.log('gewonnen');} 
+
+    else if (args == "Schere", randomElement == "Stein") {
+        console.log('verloren');} 
+
+    else if (args == "Papier", randomElement == "Stein") {
+        console.log('gewonnen');} 
+
+    else if (args == "Papier", randomElement == "Schere") {
+        console.log('verloren');} 
+
+    else if (args == "Stein", randomElement == "Schere") {
+        console.log('gewonnen');}     
+
+    else if (args == "Stein", randomElement == "Papier") {
+            console.log('verloren');}    
+
      else {
-    console.log ("verloren")
+    console.log ("no Match")
     }
+
