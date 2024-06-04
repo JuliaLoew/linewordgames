90328a28399ce65d36eb8825d5fe6e221032a43f
unknown <julia.loew@gmx.net>
2024-06-04 09:58:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Stein Papier Schere
+console.log(process.argv); 
+const args = process.argv.slice(2);
+
+
+console.log("test");
+
+const Computereingabe = "Schere";
+
+    if (args == Computereingabe) {
+        console.log('Unentschieden');
+    }
+     else {
+    console.log ("verloren")
+    }
